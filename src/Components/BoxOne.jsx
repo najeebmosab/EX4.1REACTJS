7786f0fa36384a3af6265c87d8d33ec272ad4577
najeebmosab <73877639+najeebmosab@users.noreply.github.com>
2023-02-15 14:01:36 +0200

    update EX4.1 React js to EX8.1

--- src/Components/BoxOne.jsx
+++ src/Components/BoxOne.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Box1 = () => {
     return (
-        <div style={{ backgroundColor: 'green', width: '100%', height: '100vh',textAlign:'center',display:'flex',alignItems:'center',justifyContent:'center' }}>
+        <div className='Box1' style={{ backgroundColor: 'green', width: '100%', height: '100vh', textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <Box2 />
         </div>
     );
@@ -10,7 +10,7 @@ const Box1 = () => {
 
 const Box2 = () => {
     return (
-        <div style={{ backgroundColor: 'blue', width: '95%', height: '90%', margin: 'auto auto',textAlign:'center',display:'flex',alignItems:'center',justifyContent:'center' }}>
+        <div  className='Box2' style={{ backgroundColor: 'blue', width: '95%', height: '90%', margin: 'auto auto', textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <Box3 />
         </div>
     );
@@ -18,7 +18,7 @@ const Box2 = () => {
 
 const Box3 = () => {
     return (
-        <div style={{ backgroundColor: 'pink', width: '80%', height: '80%', margin: 'auto auto',textAlign:'center',display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'column'}}>
+        <div className='Box3' style={{ backgroundColor: 'pink', width: '80%', height: '80%', margin: 'auto auto', textAlign: 'center', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
             <Box4 />
             <Box4 />
         </div>
@@ -27,7 +27,7 @@ const Box3 = () => {
 
 const Box4 = () => {
     return (
-        <div style={{ backgroundColor: 'purple', width: '50%', height: '25%', margin: '25px'}} />
+        <div className='Box4' style={{ backgroundColor: 'purple', width: '50%', height: '25%', margin: '25px' }} />
     );
 };
 
